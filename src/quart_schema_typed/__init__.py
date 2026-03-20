516bac7ce351ebe6611b480edd1dec517ced1293
Kalle M. Aagaard <git@k-moeller.dk>
2026-03-20 10:02:59 +0100

    Version updated from 0.1.2 to 0.1.3

--- src/quart_schema_typed/__init__.py
+++ src/quart_schema_typed/__init__.py
@@ -22,7 +22,7 @@ class DataclassProtocol(Protocol):
     __post_init__: Callable | None  # pyright: ignore[reportMissingTypeArgument]
 
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 
 QUERY_ARGS_KEY = "query_args"
